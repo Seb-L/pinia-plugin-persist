3ee2fecd63cc69c13e94431debc27ad9b877f010
Sean Matheson <sean@ctrlplusb.com>
2021-09-08 23:25:10 +0800

    Adds support for Vue 2/3 via vue-demi

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { watch } from '@vue/composition-api'
+import { watch } from 'vue-demi'
 import { PiniaPluginContext, StateTree, GettersTree } from 'pinia'
 
 export interface PersistStrategy {
