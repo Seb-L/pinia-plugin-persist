9fdfeffff7b16e75ef874dcb5f7f22b7627b4f30
Sébastien Lombard <sebastien.lombard75@gmail.com>
2021-09-22 10:15:15 +0200

    fix(npm): publish on github tag on master #11 (#16)

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 import packageJson from './package.json'
@@ -5,8 +6,7 @@ import packageJson from './package.json'
 const bundle = (config) => ({
   ...config,
   input: 'src/index.ts',
-  // external: (id) => !/^[./]/.test(id),
-  external: ['vue', '@vue/composition-api'],
+  external: ['vue-demi'],
 })
 
 export default [
@@ -28,7 +28,10 @@ export default [
     ],
   }),
   bundle({
-    plugins: [dts()],
+    plugins: [
+      nodeResolve(),
+      dts(),
+    ],
     output: {
       file: `dist/${packageJson.name}.d.ts`,
       format: 'es',
